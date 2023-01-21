3e6969a68aa3dcf899a4f728fc56c0faef3888d9
nicksfed <nick.guzy76@gmail.com>
2023-01-20 17:45:54 -0800

    WIP created if and if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,36 @@ window.onload = function(){
   event.preventDefault();
 
   
- 
+ const arr = []
 
 }
   }
   function build(num){
     const x = num
-    const arr = []
-    const innerTextError = "Enter a number between 1 and 9";
-    if (num <= 9){
     for(let i = 0; i <= num; i++){
       arr.push(i)
       console.log(arr);}
-      replace(arr)}
-      else {(document.getElementById("output-area").innerText = innerTextError)}
+    arbit(arr)
+}
+
+  
+
+
+function arbit(arr){
+  for(let i = 0; i <= arr.length; i++){
+    if(arr[i] = 1){
+      arr.splice(i,1,"beep!");
+    }
+    else if(arr[i] = 2){
+      arr.splice(i,1,"boop!");
+    }
+    else if(arr[i = 3]){
+      arr.splice(i,1,"Will you be my neighbor?")
+    }
   }
-
-
-function replace(arr){
-  arr.replace(1, "beep!")
-  console.log(arr)
-  arr.replace(2, "boop!")
-  console.log(arr)
-  arr.replace(3, "Will you be my neighbor?")
   console.log(arr)
   document.getElementById("output-area").innerText = arr;
 }
+
+
+
